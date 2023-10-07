2c790f860939ab6441d7fa0dbfab633c878968c8
tlacotl <56213019+tlacotl@users.noreply.github.com>
2023-10-06 21:31:11 -0500

    Add files via upload

--- lyra.py
+++ lyra.py
@@ -1,21 +1,27 @@
 import json
 import openai
 import os
+import re
 
 os.system("cls")
 
-# Set your API key
-openai.api_key = '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'
-
 # Define the conversation history file path
 conversation_history_file = "conversation_history.json"
+code_output_file = "Code Output.txt"
+api_key_file = "api_key.json"
+reset_frequency = 10
 
-# Define the directory for storing output code files
-code_dir = "output_code"
+# Read the API key from the JSON file
+try:
+    with open(api_key_file, "r") as file:
+        api_key_data = json.load(file)
+        api_key = api_key_data["api_key"]
+except (FileNotFoundError, KeyError):
+    print(f"Unable to read the API key from {api_key_file}. Please ensure the file exists and contains a valid 'api_key' field.")
+    exit(1)
 
-# Create the directory if it doesn't exist
-if not os.path.exists(code_dir):
-    os.makedirs(code_dir)
+# Set the API key
+openai.api_key = api_key
 
 # Load conversation history from file if it exists
 try:
@@ -23,12 +29,6 @@ try:
         conversation_history = json.load(file)
 except FileNotFoundError:
     conversation_history = []
-
-def count_tokens(message):
-    return len(openai.api.encoding.encode(message))
-
-# Define a maximum number of tokens for a conversation
-max_tokens = 4000
 
 while True:
     # Get user input
@@ -46,49 +46,57 @@ while True:
     # Append the user input to the conversation history
     conversation_history.append({"role": "user", "content": user_input})
 
+    # Check if the user wants to analyze a file
+    if user_input.startswith("analyze file"):
+        filename = user_input.replace("analyze file", "").strip()
+        if not filename:
+            print("Please provide a valid filename.")
+            continue
+
+        if not os.path.exists(filename):
+            print(f"File '{filename}' does not exist.")
+            continue
+
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                code_content = file.read()
+        except UnicodeDecodeError:
+            print(f"Unable to read the file '{filename}' due to an encoding issue.")
+            continue
+
+        # Prepend code content with appropriate instruction to Lyra
+        conversation_history.append({"role": "assistant", "content": f"Please analyze the code file '{filename}':"})
+
+        # Add code content to the conversation history
+        conversation_history.append({"role": "user", "content": code_content})
+
     try:
+        if len(conversation_history) > reset_frequency:
+            conversation_history = conversation_history[-reset_frequency:]
+
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=conversation_history[-max_tokens:]
+            messages=conversation_history
         )
 
         response_text = response.choices[0].message.content.strip()
-
-        # If the response is a code block, write it to a file
-        if response_text.startswith("```") and response_text.endswith("```"):
-            # Remove the backticks and extract the code
-            code = response_text[3:-3]
-
-            # Create a unique filename for the code file
-            code_file = os.path.join(code_dir, f"code_{len(os.listdir(code_dir)) + 1}.txt")
-
-            # Write the code to the file
-            with open(code_file, "w") as file:
-                file.write(code)
-
-            # Replace the response text with the file path
-            response_text = f"I have written the code to a file: {code_file}"
 
         print("Lyra: " + response_text)
 
         # Append the AI's response to the conversation history
         conversation_history.append({"role": "assistant", "content": response_text})
 
-        # Reduce conversation history to the last N tokens, keeping important messages
-        important_messages = [msg for msg in conversation_history if 'important' in msg['content']]
-        tokens_to_keep = max_tokens - sum(count_tokens(msg['content']) for msg in important_messages)
-        new_history = important_messages
+        # Save conversation history to file
+        with open(conversation_history_file, "w") as file:
+            json.dump(conversation_history, file)
 
-        for message in reversed(conversation_history):
-            if count_tokens(message['content']) <= tokens_to_keep:
-                new_history.append(message)
-                tokens_to_keep -= count_tokens(message['content'])
-                
-        conversation_history = list(reversed(new_history))
+        # Extract code from response
+        code_matches = re.findall("```(.*?)```", response_text, re.DOTALL)
+        extracted_code = "\n".join(code_matches)
+
+        # Save extracted code to separate file
+        with open(code_output_file, "w") as file:
+            file.write(extracted_code)
 
     except Exception as e:
         print("Uh oh!:", str(e))
-
-    # Save conversation history to file
-    with open(conversation_history_file, "w") as file:
-        json.dump(conversation_history, file)
